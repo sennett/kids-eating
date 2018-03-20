b893ae9dc02f9f517e45168c4ec5e65463a41776
Anthony Sennett <anthony.sennett@gmail.com>
2018-03-20 22:11:55 +0100

    moving dat to external file; making css nicer

---
+++ js/data.js
@@ -0,0 +1,155 @@
+var restaurants = [{
+    name: "Restaurant Singular",
+    address: "Carrer de Sardenya, 321, 08025 Barcelona",
+    lat: 41.403922,
+    lng: 2.172328,
+    summary: "Great play area. Rather touristy. Average food for the price.",
+    emojis: "💺🎲🍷🍷🍽️",
+    cost: "$$$",
+    blue: false
+    // highchairs, alcholo, playarea, food, cafe/restaurant, space, changing mat 💺🎲☕🥐🍷🍽️💩
+}, {
+    name: "The Pudding Bar",
+    address: "Carrer de Pau Claris, 90, 08010 Barcelona",
+    lat: 41.390185,
+    lng: 2.170693,
+    summary: "Great play area. Good for brunch. Big toadstools.",
+    emojis: "🎲☕🍷🥐🍽️",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Pudding Coffee Shop",
+    address: "Avinguda Diagonal, 515, 08029 Barcelona",
+    lat: 41.391311,
+    lng: 2.140709,
+    summary: "Great play area. Good for brunch. Harry Potter themed.",
+    emojis: "🎲☕🍷🥐🍽️",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Family Room Cafe",
+    address: "Carrer de Joan Gamper, 13, 08014 Barcelona",
+    lat: 41.387115,
+    lng: 2.133945,
+    summary: "Great play area. Small but tasty menu.",
+    emojis: "🎲☕🍷🥐🍽️💩💩",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Family Room Cafe",
+    address: "Calle Vergós 55 (plaza), 08017 Barcelona",
+    lat: 41.396830,
+    lng: 2.126869,
+    summary: "Great play area. Small but tasty menu.",
+    emojis: "🎲☕🍷🥐🍽️💩💩",
+    cost: "$$",
+    blue: false
+}, {
+    name: "+ Calida:d Fleca i degustació",
+    address: "Carrer del Rosselló, 360, 08025 Barcelona",
+    lat: 41.403563,
+    lng: 2.171249,
+    summary: "Great play area. Good tea selection. Pastry-style snacks only.",
+    emojis: "🎲☕🥐",
+    cost: "$$",
+    blue: false
+}, {
+    name: "El Señorio del Perú",
+    address: "Carrer de Còrsega, 607, 08025 Barcelona",
+    lat: 41.408517,
+    lng: 2.175085,
+    summary: "Nice food. Chilled out.  Lunch/dinner.",
+    emojis: "💺🍷🍽️",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Doble Zer00 Restaurant",
+    address: "Carrer de Jaume Giralt, 53, 08003 Barcelona",
+    lat: 41.387935,
+    lng: 2.178699,
+    summary: "Good sushi. Excellent child-care. Lunch/dinner.",
+    emojis: "💺💺🎲🎲🍷🍷🍽️🍽️☕",
+    cost: "$$$",
+    blue: false
+}, {
+    name: "Restaurante Bar Cel Ona",
+    address: "Carrer de la Cirera, 4, 08003 Barcelona, Spain",
+    lat: 41.385143,
+    lng: 2.181925,
+    blue: true
+}, {
+    name: "Bitacora tapas",
+    address: "Plaça de la Unió, 24, 08005 Barcelona",
+    lat: 41.400914,
+    lng: 2.204891,
+    summary: "Good food. Lots of space.",
+    emojis: "💺🍷🍷🍽️☕💩",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Al Waha Restaurant",
+    address: "Carrer de Verdi, 16, 08012 Barcelona",
+    lat: 41.403489,
+    lng: 2.157335,  
+    blue: true
+}, {
+    name: "BANNA, Restaurant Thai",
+    address: "Plaça Revolució de Setembre 1868, 15, 08012 Barcelona",
+    lat: 41.402534,
+    lng: 2.158083,
+    summary: "Reasonable Thai food. Walled playpark outside.",
+    emojis: "🎲🍷🍽️☕",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Mama's Cafe",
+    address: "Carrer de Torrijos, 26, 08012 Barcelona",
+    lat: 41.403251,
+    lng: 2.159088,
+    summary: "Good for brunch. Nice outdoors area.",
+    emojis: "🎲🍷🍽️☕",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Cachitos Café",
+    address: "Carrer de Roger de Flor, 204, 08013 Barcelona",
+    lat: 41.399137,
+    lng: 2.172376,
+    summary: "Good for brunch. Lovely lady running it. Really amazing place.",
+    emojis: "☕🥐🥐🍽️",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Café Lennon",
+    address: "Carrer de Puigmartí, 35, 08012 Barcelona",
+    lat: 41.402840,
+    lng: 2.161110,
+    summary: "Snacks. Outside. Next to a gated playpark.",
+    emojis: "☕🎲🍷🥐️",
+    cost: "$$",
+    blue: false
+}, {
+    name: "Bar del Convent",
+    address: "Carrer del Comerç, 36, 08003 Barcelona",
+    lat: 41.387820,
+    lng: 2.181603,
+    blue: true
+}, {
+    name: "Chocolatería La Nena",
+    address: "Carrer de Ramón y Cajal, 36, 08012 Barcelona",
+    lat: 41.402552,
+    lng: 2.158858,
+    blue: true
+}, {
+    name: "Bobo Pulpín",
+    address: "Carrer de la Freneria, 5, 08002 Barcelona",
+    lat: 41.383703,
+    lng: 2.177346,
+    blue: true
+}, {
+    name: "Kasparo",
+    address: "Plaça de Vicenç Martorell, 4, 08001 Barcelona",
+    lat: 41.384469,
+    lng: 2.168990,
+    blue: true
+} ]
