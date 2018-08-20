7f1f6414cc68eb310fc4b44d4d32a63c002b85b4
Anthony Sennett <anthony.sennett@gmail.com>
2018-08-20 15:28:20 -0500

    close button

--- js/help-screen.js
+++ js/help-screen.js
@@ -6,6 +6,15 @@ function initHelpScreen () {
             .without("help-screen--closed")
             .concat("help-screen--open")
             .join(" ")
-        
     }
+    
+    document.getElementById('closeHelpScreen').onclick = function () {
+        const helpScreen = document.getElementById('helpScreen')
+        helpScreen.className = _(helpScreen.className)
+            .split(" ")
+            .without("help-screen--open")
+            .concat("help-screen--closed")
+            .join(" ")
+        
+    }    
 }
