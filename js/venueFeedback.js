1a14bc52bdecf2884eb5691f5518e996deec298b
Anthony Sennett <anthony.sennett@gmail.com>
2018-08-21 15:41:40 -0500

    venue feedback

---
+++ js/venueFeedback.js
@@ -0,0 +1,15 @@
+function openVenueFeedback (venueName, venueAddress) {
+    const tfUrl = `https://sennett.typeform.com/to/cCijNO?placename=${encodeURIComponent(venueName)}&placeaddress=${encodeURIComponent(venueAddress)}`
+    const embedInstance = typeformEmbed.makePopup(tfUrl, {
+        autoClose: 3000, // not working
+        hideHeaders: true, // neither of these work
+        hideFooters: true, // neither of these work
+        // autoclose not working :D
+        onSubmit: function () {
+            setTimeout(function () {
+                embedInstance.close()
+            }, 3000)
+        }
+    })
+    embedInstance.open()
+}
